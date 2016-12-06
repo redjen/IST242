5b9826d6fb1e312d28dac09b0bcae6d78327b4df
Jennifer Mullen <jsm158@psu.edu>
2016-12-05 22:24:57 -0800

    Removed dead code.

--- Final/src/game/GameController.java
+++ Final/src/game/GameController.java
@@ -386,28 +386,4 @@ public class GameController {
       resetGame();
    }
 
-   /**
-    * Shows a pop-up message at the start of the game.
-    */
-   private void showWelcomeMessage() {
-
-      JOptionPane.showMessageDialog(null,
-              "<html><body style='width: 500px'><p>Welcome to my final project!</p><p></p>"
-              + "<p>I've added a challenge mode in which the goal is to score as many "
-              + "touchdowns as possible before being tackled.</p><p></p>"
-              + "<p>The game will also now keep a high score log that persists "
-              + "across sessions.</p></body></html>",
-              "Welcome!", JOptionPane.INFORMATION_MESSAGE);
-   }
-
-   /**
-    * Shows the game over message and prompts the user for their name.
-    *
-    * @return the user's input
-    */
-   private String showGameOverMessage() {
-      String name = JOptionPane.showInputDialog(null, "Please enter your name.", "Game over!", 0);
-      return name;
-   }
-
 }
